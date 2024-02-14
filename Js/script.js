bfc603dc07ddfbe173bd9920f351f6cc43cafe1c
rajputsushil <rajputsushil904@gmail.com>
2024-02-14 10:40:46 +0530

    another update

--- Js/script.js
+++
@@ -1,9 +0,0 @@
-const Con =document.querySelector("#image");
-const hearticon =document.querySelector("i");
-
-Con.addEventListener('dblclick',function(){
-    hearticon.style.transform ='translate(-50%,-50%) scale(4)'
-    setTimeout(() => {
-        hearticon.style.transform ='translate(-50%,-50%) scale(0)'        
-    }, 1000);
-})
